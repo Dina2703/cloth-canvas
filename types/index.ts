50098dd559d8e492992c27ae133cbae35e3d7b6e
Dinara Idrissova <Dinara.Idrissova@list.ru>
2024-03-29 11:36:54 -0400

    SearchBar component structure

--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,8 @@ export interface CustomBtnProps {
   btnType?: "button" | "submit";
   handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
+
+export interface SearchManufacturerProps {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => avoid;
+}
